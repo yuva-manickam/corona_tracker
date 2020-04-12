e8fdadcc61872eb3de570864045724afb0c702f9
Yuvaraj M <yuva.manickam@gmail.com>
2020-04-12 14:01:50 +0530

    main method removed

--- src/main/java/com/vyb/coronatracker/CoronaTrackerApplication.java
+++ src/main/java/com/vyb/coronatracker/CoronaTrackerApplication.java
@@ -7,8 +7,5 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class CoronaTrackerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoronaTrackerApplication.class, args);
-	}
 
 }
