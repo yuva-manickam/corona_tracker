18d2f6f222a3c67a9297306250fb9cc388c77f06
Yuvaraj M <yuva.manickam@gmail.com>
2020-04-12 13:46:03 +0530

    changes done in CoronaTrackerApplication

--- src/main/java/com/vyb/coronatracker/CoronaTrackerApplication.java
+++ src/main/java/com/vyb/coronatracker/CoronaTrackerApplication.java
@@ -2,9 +2,10 @@ package com.vyb.coronatracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CoronaTrackerApplication {
+public class CoronaTrackerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaTrackerApplication.class, args);
